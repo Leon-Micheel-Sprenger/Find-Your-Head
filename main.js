8955e5c14e828b68e26874ebd2ab6616f4420ce9
Leon Micheel-Sprenger <l.micheelsprenger@gmail.com>
2021-06-19 21:58:31 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,149 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+const hat = '^';
+const hole = 'O';
+const fieldCharacter = '░';
+const pathCharacter = '*';
+
+
+
+
+
+//Class
+
+class Field {
+    constructor(field){
+        this.field = field;
+        this.indexPosX = 0 ;
+        this.indexPosY = 0;
+    }
+
+    print(){
+        //put * where index is
+        this.field[this.indexPosY][this.indexPosX] = pathCharacter;
+    
+        //print the field
+        for (let i=0; i<this.field.length; i++){
+            console.log(this.field[i].join(" "));
+        }       
+    }
+
+
+    move(letter){
+        switch(letter){
+            case "d":
+                    this.indexPosY += 1;
+                break;
+
+            case "r": 
+                    this.indexPosX += 1;
+                break;
+
+            case "u":
+                    this.indexPosY -= 1;
+                break;
+
+            case "l": 
+                    this.indexPosX -= 1;
+                break;
+            default:
+                //nothing
+        }
+    }
+
+
+//generate a new random field
+    static generateField(x,y,percentageOfHoles){
+
+        let fieldArr = [];
+
+        let reqNumOfHoles = percentageOfHoles * (x*y);
+        let numOfHoles = 0;
+
+        for (let i=0; i<y; i++){
+            fieldArr[i] = [];
+            for (let j=0; j<x; j++){
+                    fieldArr[i][j] = fieldCharacter;
+            }
+        }
+
+    
+
+        //generate reqNumOfHoles different numbers betwen 0 and x/y;
+        for (let h=0; h<reqNumOfHoles; h++){
+
+                let numX = Math.floor(Math.random()*x);
+                let numY = Math.floor(Math.random()*y)
+                
+                fieldArr[numY][numX] = hole; 
+
+                numOfHoles++;
+            
+        }
+
+
+        //put hat in random place: 
+        let hatNumX = Math.floor(Math.random()*x);
+        let hatNumY = Math.floor(Math.random()*y);
+
+        fieldArr[hatNumX][hatNumY] = hat;
+
+        //return fieldArr
+        return fieldArr;
+
+    }
+
+
+}
+
+
+function endGame(x, y, field){
+
+    //move outside of the field
+    if (y > field.length-1 || y < 0 || x > field[0].length-1 || x < 0){
+        console.log('You moved outside the field');
+        return false;
+    }
+    //move on a hole
+    else if (field[y][x] === hole){
+        console.log('You stepped on a hole');
+        return false;
+    }
+    //move on hat
+    else if (field[y][x] === hat){
+        console.log('Congratulations, you got your head!');
+        return false;
+    }
+    else {
+       return true;
+    }
+}
+
+
+
+//generate random field array
+const fieldArr= Field.generateField(10,10,0.2); 
+//console.log(fieldArr)
+
+//create field:
+const newField = new Field(fieldArr);
+
+
+
+//loop to play:
+while(endGame(newField.indexPosX, newField.indexPosY, newField.field) === true){
+    newField.print();
+    const letter = prompt('Which direction? ');
+    newField.move(letter);
+} 
+
+
+
+
+
+
+
+
+
+
+
